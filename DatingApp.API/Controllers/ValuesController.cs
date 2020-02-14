9a5dfde485fe17c5a582da3ad9160463e77617ee
Jonathan Burton <jonathan.burton@codaoctopus.com>
2020-02-14 09:15:23 +0000

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context, ILogger<ValuesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var value = await _context.Values.Where(v => v.Id == id).FirstOrDefaultAsync();
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+
+        [HttpPut("id")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        [HttpDelete("id")]
+        public void Delete(int id)
+        {
+
+        }
+
+
+        private readonly ILogger<ValuesController> _logger;
+    }
+}
